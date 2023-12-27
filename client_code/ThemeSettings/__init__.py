aab135418ba3c3597563147cee76f2cb615c5f66
03mulcahyj@gmail.com <03mulcahyj@gmail.com>
2023-12-27 19:08:46 +0000

    Edited ThemeSettings and SetLocation

--- client_code/ThemeSettings/__init__.py
+++ client_code/ThemeSettings/__init__.py
@@ -8,3 +8,8 @@ class ThemeSettings(ThemeSettingsTemplate):
     self.init_components(**properties)
 
     # Any code you write here will run before the form opens.
+
+  def backButton_click(self, **event_args):
+    """This method is called when the button is clicked"""
+    open_form('HomePage')
+    pass
