f6c1fd7d04e6e6e63d4c7cf1bbedf26a45e38c10
03mulcahyj@gmail.com <03mulcahyj@gmail.com>
2024-01-06 22:55:18 +0000

    Created ColourPicker; Edited ThemeSettings

--- client_code/ThemeSettings/__init__.py
+++ client_code/ThemeSettings/__init__.py
@@ -6,6 +6,8 @@ class ThemeSettings(ThemeSettingsTemplate):
   def __init__(self, **properties):
     # Set Form properties and Data Bindings.
     self.init_components(**properties)
+    self.ColorPicker.set_color('#ff0000')
+    self._update_label()
 
     # Any code you write here will run before the form opens.
 
@@ -14,3 +16,9 @@ class ThemeSettings(ThemeSettingsTemplate):
     open_form('Settings')
     print("[INFO]... Returning to Settings")
     pass
+
+  def colorpicker_change(self, **event_args):
+    self._update_label()
+    
+  def _update_label(self):
+    self.label_1.foreground = self.ColorPicker.get_color()
