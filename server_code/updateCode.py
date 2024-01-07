3e533a07e8b53831c9434628d5b63745c750eb4e
03mulcahyj <154929350+03mulcahyj@users.noreply.github.com>
2024-01-07 20:03:51 +0000

    Update updateCode.py
    Corrected typo in alternative filepath

--- server_code/updateCode.py
+++ server_code/updateCode.py
@@ -6,7 +6,7 @@ def gitPull():
     if os.path.isfile('/home/pi/auxilaryCode/gitUpdate.sh') is True:
         result = subprocess.run(['sudo', 'sh', '/home/pi/auxilaryCode/gitUpdate.sh'],
                                 capture_output=True,text=True)
-    elif os.path.isfile('/home/pi//home/pi/weatherApp/gitUpdate.sh') is True:
+    elif os.path.isfile('/home/pi/weatherApp/gitUpdate.sh') is True:
         result = subprocess.run(['sudo', 'sh', '/home/pi/weatherApp/gitUpdate.sh'],
                                 capture_output=True,text=True)
     print("[INFO]... git command run. Output below:")
