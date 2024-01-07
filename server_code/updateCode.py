69f875c23411fd2c0a2ad5f4e05ea7108d75882c
03mulcahyj <03mulcahyj@gmail.com>
2024-01-07 00:28:55 +0000

    Changing update code to look at a different directory

--- server_code/updateCode.py
+++ server_code/updateCode.py
@@ -2,6 +2,6 @@ import subprocess
 
 def gitPull():
     print("[INFO]... Updating git software")
-    result = subprocess.run(['sudo', 'sh', '/home/pi/weatherApp/gitUpdate.sh'],
+    result = subprocess.run(['sudo', 'sh', '/home/pi/auxilaryCode/gitUpdate.sh'],
                             capture_output=True,text=True)
     return result.stdout
