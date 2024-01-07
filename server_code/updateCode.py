72a76354b417f3c0ffb5ff975232b55e11494c16
03mulcahyj <03mulcahyj@gmail.com>
2024-01-07 00:54:17 +0000

    Changes to updateCode.py to include useful error messages

--- server_code/updateCode.py
+++ server_code/updateCode.py
@@ -4,4 +4,6 @@ def gitPull():
     print("[INFO]... Updating git software")
     result = subprocess.run(['sudo', 'sh', '/home/pi/auxilaryCode/gitUpdate.sh'],
                             capture_output=True,text=True)
+    print("[INFO]... git command run. Output below:")
+    print(result)
     return result.stdout
